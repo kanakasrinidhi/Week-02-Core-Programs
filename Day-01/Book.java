2e56c72cc92de334ac518ffc8377eb92c18d6e53
[Kanaka Srinidhi] <[bkanakasrinidhi@gmail.com]>
2025-04-10 10:00:00 +0530

    add files

---
+++ Day-01/Book.java
@@ -0,0 +1,35 @@
+package ClassObj;
+
+import java.util.Scanner;
+
+class Book {
+    String title, author;
+    double price;
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("\nBook Details:");
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: $" + price);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Book Title: ");
+        String title = sc.nextLine();
+        System.out.print("Enter Author Name: ");
+        String author = sc.nextLine();
+        System.out.print("Enter Book Price: ");
+        double price = sc.nextDouble();
+
+        Book book = new Book(title, author, price);
+        book.displayDetails();
+        sc.close();
+    }
+}
---
+++ Day-01/Employee.java
@@ -0,0 +1,36 @@
+package ClassObj;
+
+import java.util.Scanner;
+
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("\nEmployee Details:");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: $" + salary);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Employee Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter Employee ID: ");
+        int id = sc.nextInt();
+        System.out.print("Enter Employee Salary: ");
+        double salary = sc.nextDouble();
+
+        Employee emp = new Employee(name, id, salary);
+        emp.displayDetails();
+        sc.close();
+    }
+}
