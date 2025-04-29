4ebf8cd3d242ab07404aa3dc3d57c6cd7e2e0c35
[Kanaka Srinidhi] <[bkanakasrinidhi@gmail.com]>
2025-04-29 12:05:28 +0530

    day 03

---
+++ Day-03/Book.java
@@ -0,0 +1,41 @@
+package StaticInstanceopkeywords;
+
+import java.util.Scanner;
+
+class Book {
+    private static String libraryName = "City Library";
+    private final String isbn;
+    private String title, author;
+
+    Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    static void displayLibraryName() {
+        System.out.println("Library Name: " + libraryName);
+    }
+
+    void displayDetails() {
+        if (this instanceof Book) {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("ISBN: " + isbn);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Book Title: ");
+        String title = sc.nextLine();
+        System.out.print("Enter Author Name: ");
+        String author = sc.nextLine();
+        System.out.print("Enter ISBN: ");
+        String isbn = sc.nextLine();
+
+        Book book = new Book(title, author, isbn);
+        displayLibraryName();
+        book.displayDetails();
+    }
+}
